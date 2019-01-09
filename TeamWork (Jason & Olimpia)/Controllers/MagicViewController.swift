7df7ed64d853ea992e7390d725327dcccda31e03
Jason <Jason@Pursuits-MacBook-Pro-42.local>
2019-01-09 14:58:28 -0500

    initial setup

---
+++ TeamWork (Jason & Olimpia)/Controllers/MagicViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TeamWork (Jason & Olimpia)
+//
+//  Created by Olimpia on 1/9/19.
+//  Copyright © 2019 Olimpia. All rights reserved.
+//
+
+import UIKit
+
+class MagicViewController: UIViewController {
+
+    @IBOutlet weak var MagicCollectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
